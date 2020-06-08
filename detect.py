4b6efb39b070425fd2c8235a2d90280f72fb5139
Emmanuel <enggadas80@gmail.com>
2020-06-08 12:59:46 +0100

    Initial commit

---
+++ detect.py
@@ -0,0 +1,7 @@
+from copy_move_detection import detect
+
+# detect.detect('assets/cattle_copy.png', 'output/')
+# detect.detect('assets/clean_walls_copy.png', 'output/')
+# detect.detect('assets/giraffe_copy.png', 'output/')
+# detect.detect('assets/horse_fake.png', 'output/')
+detect.detect('assets/test_copy.png', 'output/')
